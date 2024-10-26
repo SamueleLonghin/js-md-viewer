b541b25fb1fe3f355f5b437cbd14ced4121a69f9
Samuele <46517478+SamueleLonghin@users.noreply.github.com>
2024-10-26 16:47:49 +0200

    toggle sidebar quando sono in mobile e clicco sul menù

--- public/script-php.js
+++ public/script-php.js
@@ -3,8 +3,16 @@ const sidebar = document.getElementById('sidebar');
 const sidebarToggle = document.getElementById('sidebar-toggle');
 sidebarToggle.addEventListener('click', toggleSidebar);
 
+
+
 function toggleSidebar() {
     sidebar.classList.toggle('visible');
 }
 
 displayMarkdownContent(content)
+
+const chaptersLinks = document.getElementsByClassName('chapter-link');
+
+for (var i = 0; i < chaptersLinks.length; i++) {
+    chaptersLinks[i].addEventListener('click', toggleSidebar, false);
+}
