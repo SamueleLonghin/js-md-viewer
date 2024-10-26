5b414a0029d9f877b9a0daa504cc0ab4cc611ece
Samuele <46517478+SamueleLonghin@users.noreply.github.com>
2024-10-26 17:38:29 +0200

    pulizia, possibilità di inserire immagini utilizzando placeholder %%BASE_FOLDER%% per path

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 
-// Funzione per caricare il file config.json e restituire i dati
+// Funzione per caricare il file topics.json e restituire i dati
 function loadConfig()
 {
-    $configPath = 'risorse/topics.json'; // Assumi che il file config.json si trovi nella stessa cartella di index.php
+    $configPath = 'risorse/topics.json'; // Assumi che il file topics.json si trovi nella stessa cartella di index.php
     if (file_exists($configPath)) {
         $configContent = file_get_contents($configPath);
         return json_decode($configContent, true); // Decodifica il JSON in un array PHP
@@ -12,20 +12,15 @@ function loadConfig()
     }
 }
 
-
-// Funzione per ottenere il file markdown dal config.json
-function getFilePath($config, $macroargomento, $argomento)
-{
+function getFileFolderAndName($config, $macroargomento, $argomento){
     if (isset($config['topics'][$macroargomento]['chapters'][$argomento])) {
         $fileName = $config['topics'][$macroargomento]['chapters'][$argomento]['file']; // Prendi il nome del file markdown
         $topicFolder = $config['topics'][$macroargomento]['folder']; // Prendi il nome del file markdown
         $resourcesFolder = $config['resources-folder']; // Prendi la cartella delle risorse
-        return '.' . $resourcesFolder . $topicFolder . '/' . $fileName; // Restituisci il percorso completo del file
+        return ['.' . $resourcesFolder . $topicFolder, $fileName]; // Restituisci il percorso completo del file
     }
     return null;
 }
-
-
 
 // Funzione per caricare il contenuto del file markdown
 function getMarkdownContent($path)
@@ -33,8 +28,7 @@ function getMarkdownContent($path)
     if (file_exists($path)) {
         $content = file_get_contents($path); // Ritorna il contenuto del file markdown
         $type = pathinfo($path, PATHINFO_EXTENSION);
-        // var_dump($type);
-        // die();
+
         if($type == "ipynb"){
             require "converters.php";
             $content = convertNotebookToMd($content);
@@ -43,6 +37,10 @@ function getMarkdownContent($path)
     } else {
         return 'Errore: file non trovato! (' . $path . ")";
     }
+}
+
+function addLinks($md, $basePath){
+    return str_replace("%%BASE_FOLDER%%", $basePath, $md);
 }
 
 
@@ -59,19 +57,13 @@ function getRoute()
     $parts = explode('/', trim($path, '/'));
 
     // Se ci sono almeno due parti, il primo è il macroargomento e il secondo è l'argomento
-    if (count($parts) >= 2) {
-        return [
-            'macroargomento' => urldecode($parts[0]),  // Prima parte dell'URL
-            'argomento' => urldecode($parts[1])        // Seconda parte dell'URL
-        ];
-    } else {
-        if (count($parts) == 1) {
-            return ['macroargomento' => urldecode($parts[0])];
-        }
-    }
-
-    // Se l'URL non è completo, restituisci null
-    return null;
+    if (count($parts) >= 2) 
+        return [urldecode($parts[0]),urldecode($parts[1])];
+    // Se ce c'è una soltanto è il macroargomento
+    else if(count($parts) == 1)
+        return [urldecode($parts[0]),null];
+    // Se è vuoto niente
+    return [null,null];
 }
 
 function valOrDefault($dict, $key, $default = null)
@@ -87,30 +79,33 @@ if (!$config) {
     die('Errore: Impossibile caricare il file di configurazione.');
 }
 
-// Estrai macroargomento e argomento dall'URL
-
+// Path nella quale è posizionato il servizio (nel caso vengano utilizzati .htaccess per redirect può tornare utile)
 $BASE_PATH = valOrDefault($config, "base-path", '/');
 
-$route = getRoute();
+// Estrai macroargomento e argomento dall'URL
+list($macroargomento, $argomento) = getRoute();
+
 $markdownContent = '';
+// Enable preview
 $preview = valOrDefault($config, "preview", false);
+// Default language
 $language = valOrDefault($config, "language");
 
-$macroargomento = valOrDefault($route, 'macroargomento');
 
 if ($macroargomento && isset($config['topics'][$macroargomento])) {
-    $argomento = valOrDefault($route, 'argomento');
 
     $preview = valOrDefault($config['topics'][$macroargomento], "preview", $preview);
     $language = valOrDefault($config['topics'][$macroargomento], "language", $language);
 
     if ($argomento && isset($config['topics'][$macroargomento]['chapters'][$argomento])) {
-        // Ottieni il percorso del file markdown dal config.json
-        $markdownPath = getFilePath($config, $macroargomento, $argomento);
+        // Ottieni il percorso del file markdown dal topics.json
+        $fileFolderAndName = getFileFolderAndName($config, $macroargomento, $argomento);
 
         // Carica il contenuto markdown se il percorso è valido
-        if ($markdownPath) {
+        if ($fileFolderAndName) {
+            $markdownPath = implode("/", $fileFolderAndName);
             $markdownContent = getMarkdownContent($markdownPath);
+            $markdownContent = addLinks($markdownContent,$BASE_PATH.$fileFolderAndName[0]);
         } else {
             $markdownContent = 'Errore: Argomento non trovato nel file di configurazione.';
         }
