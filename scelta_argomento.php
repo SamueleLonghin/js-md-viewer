addf9f69b03c7c12444cc0f638a89e3f0e52daab
Samuele <46517478+SamueleLonghin@users.noreply.github.com>
2024-11-04 15:13:23 +0100

    Visibilità

--- scelta_argomento.php
+++ scelta_argomento.php
@@ -4,8 +4,9 @@ global $topics, $BASE_PATH, $macroargomento, $title;
 
 require "head.php";
 ?>
+
 <body>
-    
+
     <header class="sticky-top bg-white  mb-2 p-3 ">
         <a href="/corsi" class=" btn btn-outline-secondary position-absolute ">
             <i class="bi bi-arrow-left"></i>
@@ -16,24 +17,29 @@ require "head.php";
 
     <div class="container">
         <div class="row">
-            <?php foreach ($topics[$macroargomento]['chapters'] as $argomentoKey => $chapter): ?>
-                <div class=" col-12 col-sm-6 col-md-4 col-lg-3 mb-4">
-                    <div class="card h-100">
-                        <img src="<?= $chapter['cover_image'] ?>" class="card-img-top" alt="<?= $argomentoKey ?>">
-                        <div class="card-body">
-                            <h5 class="card-title"><?= $argomentoKey ?></h5>
-                            <p class="card-text"><?= $chapter['description'] ?></p>
-                            <a href="/<?= urlencode($macroargomento). '/' . urlencode($argomentoKey) ?>" class="stretched-link"></a>
+            <?php foreach ($topics[$macroargomento]['chapters'] as $argomentoKey => $chapter) {
+                if (!isset($chapter['visibility']) || $chapter['visibility'] == 'visible') {
+                    ?>
+                    <div class=" col-12 col-sm-6 col-md-4 col-lg-3 mb-4">
+                        <div class="card h-100">
+                            <img src="<?= $chapter['cover_image'] ?>" class="card-img-top" alt="<?= $argomentoKey ?>">
+                            <div class="card-body">
+                                <h5 class="card-title"><?= $argomentoKey ?></h5>
+                                <p class="card-text"><?= $chapter['description'] ?></p>
+                                <a href="/<?= urlencode($macroargomento) . '/' . urlencode($argomentoKey) ?>"
+                                    class="stretched-link"></a>
+                            </div>
                         </div>
                     </div>
-                </div>
-            <?php endforeach; ?>
+                <?php }
+            } ?>
         </div>
     </div>
     <!-- <a href="/" class="btn btn-outline-secondary back-button">
         <i class="bi bi-arrow-left"></i> Torna ai Corsi
     </a> -->
-    <?php require "footer.php"?>
+    <?php require "footer.php" ?>
 
 </body>
+
 </html>
