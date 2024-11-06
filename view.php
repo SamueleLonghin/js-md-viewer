4ba2cdc2235d2b9b0c32750c855f0ce896ee6dcb
Samuele <46517478+SamueleLonghin@users.noreply.github.com>
2024-11-06 08:34:55 +0100

    Modifica di indentazione

--- view.php
+++ view.php
@@ -8,53 +8,50 @@ require "head.php";
 <body>
     <button id="sidebar-toggle" class="btn btn-outline-secondary"> ☰ </button>
 
-    <div class="">
+    <div>
+        <div class="position-fixed overflow-x-auto p-3 overflow-y-auto" id="sidebar">
+            <div class="flex-column flex-nowrap vh-100 ">
+                <div class="file-section">
 
-        <div class=" ">
-            <div class="position-fixed overflow-x-auto p-3 overflow-y-auto" id="sidebar">
-                <div class="flex-column flex-nowrap vh-100 ">
-                    <div class="file-section">
+                    <a href="/corsi" class=" btn btn-outline-secondary btn-back">
+                        <i class="bi bi-arrow-left"></i>
+                        <span class="">Torna ai Corsi</span>
+                    </a>
 
-                        <a href="/corsi" class=" btn btn-outline-secondary btn-back">
-                            <i class="bi bi-arrow-left"></i>
-                            <span class="">Torna ai Corsi</span>
-                        </a>
+                    <h2>Documenti</h2>
+                    <div id="fileList">
+                        <?php
+                        if ($macroargomento) {
+                            $macroArgomentoLink = urlencode($macroargomento);
 
-                        <h2>Documenti</h2>
-                        <div id="fileList">
-                            <?php
-                            if ($macroargomento) {
-                                $macroArgomentoLink = urlencode($macroargomento);
-
-                                foreach ($topics[$macroargomento]['chapters'] as $label => $data) {
-                                    if (!isset($data['visibility']) || $data['visibility'] == 'visible') {
-                                        $link = urlencode($label);
-                                        echo "<a href='/{$macroArgomentoLink}/$link' class='text-reset file-link'> {$label} </a>";
-                                    }
-                                }
-                            } else {
-                                foreach ($topics as $topic => $val) {
-                                    $label = $val['label'];
-                                    $link = urlencode($topic);
-                                    echo "<a href='$link' class='text-reset file-link'> {$label} </a>";
+                            foreach ($topics[$macroargomento]['chapters'] as $label => $data) {
+                                if (!isset($data['visibility']) || $data['visibility'] == 'visible') {
+                                    $link = urlencode($label);
+                                    echo "<a href='/{$macroArgomentoLink}/$link' class='text-reset file-link'> {$label} </a>";
                                 }
                             }
-                            ?>
-                        </div>
-                    </div>
-                    <div class="chapters-section <?= !$argomento ? 'd-none' : '' ?>">
-                        <h3>Capitoli</h3>
-                        <div id="chapterList"></div>
+                        } else {
+                            foreach ($topics as $topic => $val) {
+                                $label = $val['label'];
+                                $link = urlencode($topic);
+                                echo "<a href='$link' class='text-reset file-link'> {$label} </a>";
+                            }
+                        }
+                        ?>
                     </div>
                 </div>
-
-
-
+                <div class="chapters-section <?= !$argomento ? 'd-none' : '' ?>">
+                    <h3>Capitoli</h3>
+                    <div id="chapterList"></div>
+                </div>
             </div>
-            <div class="col" id="content">
-                <div id="output" class="bordered"></div>
-                <?php require "footer.php" ?>
-            </div>
+
+
+
+        </div>
+        <div class="col" id="content">
+            <div id="output" class="bordered"></div>
+            <?php require "footer.php" ?>
         </div>
     </div>
 
