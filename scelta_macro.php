7c6fd1f03fb2d9b4d6a53b4203e6e059def40bcd
Samuele <46517478+SamueleLonghin@users.noreply.github.com>
2024-12-24 09:08:49 +0100

    casino con alt

--- scelta_macro.php
+++ scelta_macro.php
@@ -12,20 +12,21 @@ require "head.php";
             <?php foreach ($topics as $key => $topic): ?>
                 <div class="col-6 col-sm-4 col-md-4 mb-4">
                     <div class="card h-100">
-                    <img src="<?= addLinks($chapter['cover_image'], $BASE_PATH . $resourcesFolder . $topic['folder'] ?: "") ?>"
-                    class="card-img-top" alt="<?= $argomentoKey ?>">
-                        <img src="<?= $topic['cover_image'] ?>" class="card-img-top" alt="<?= $topic['label'] ?>">
+                        <img src="<?= addLinks($chapter['cover_image'], $BASE_PATH . $resourcesFolder . $topic['folder'] ?: "") ?>"
+                            class="card-img-top" alt="<?= $topic['label'] ?>">
+                        <!-- <img src="<?= $topic['cover_image'] ?>" class="card-img-top" alt="<?= $topic['label'] ?>"> -->
                         <div class="card-body">
                             <h5 class="card-title"><?= $topic['label'] ?></h5>
                             <p class="card-text"><?= $topic['description'] ?></p>
-                            <a href="/<?=  urlencode($key) ?>" class=" stretched-link"></a>
+                            <a href="/<?= urlencode($key) ?>" class=" stretched-link"></a>
                         </div>
                     </div>
                 </div>
             <?php endforeach; ?>
         </div>
     </div>
-    <?php require "footer.php"?>
+    <?php require "footer.php" ?>
 
 </body>
+
 </html>
