16b4db4845ebde7e155cb7fe1495ada648dba7e7
Samuele <46517478+SamueleLonghin@users.noreply.github.com>
2024-12-26 14:54:24 +0100

    fixing danno di prima

--- head.php
+++ head.php
@@ -11,7 +11,10 @@ global $argomento,$title, $BASE_PATH;
     <meta name="author" content="Samuele Longhin">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="<?=$BASE_PATH?>public/prism.css">
-    <link rel="stylesheet" href="<?=$BASE_PATH?>public/site.css">
+    <!-- <link rel="stylesheet" href="<?=$BASE_PATH?>public/site.css"> -->
+    <link rel="stylesheet" href="<?=$BASE_PATH?>public/components.css">
+    <link rel="stylesheet" href="<?=$BASE_PATH?>public/app.css">
+    <link rel="stylesheet" href="<?=$BASE_PATH?>public/print.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.11.3/font/bootstrap-icons.min.css" integrity="sha512-dPXYcDub/aeb08c63jRq/k6GaKccl256JQy/AnOq7CAnEZ9FzSL9wSbcZkMp4R26vBsMLFYH4kQ67/bbV8XaCQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
