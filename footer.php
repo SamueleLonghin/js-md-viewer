48bd4f8dd3eff200bc7087b8e9eca52b2b1e3c58
Samuele <46517478+SamueleLonghin@users.noreply.github.com>
2025-03-19 14:54:09 +0100

    aggiunto js

--- footer.php
+++ footer.php
@@ -23,6 +23,7 @@ if ($ANALYTICS_ID) {
     <?php }
     // Se il consenso non è dato, mostro il banner per accettare i cookies
     else { ?>
+        <script src='<?= $BASE_PATH ?>public/analytics.js'></script>
         <div class="cookie-consent-container" id='cookie-consent-banner'>
             Questo sito utilizza cookie per raccogliere dati statistici.
             <button id='accept-cookies'>Accetta</button>
